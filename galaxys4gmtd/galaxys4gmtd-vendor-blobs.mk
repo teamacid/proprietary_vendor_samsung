ced21075a1d55eeb4f8dbb70a42f7e6d0b641df0
Bryan Hundven <bryanhundven@gmail.com>
2012-03-25 17:23:08 -0700

    Add system/lib/libganril.so
    Change-Id: I12845d2c8adc545f8ee5dfb457ff7a594da76e70

--- galaxys4gmtd/galaxys4gmtd-vendor-blobs.mk
+++ galaxys4gmtd/galaxys4gmtd-vendor-blobs.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxys4gmtd/proprietary/libril.so:system/lib/libril.so \
     vendor/samsung/galaxys4gmtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/galaxys4gmtd/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/galaxys4gmtd/proprietary/libganril.so:system/lib/libganril.so \
     vendor/samsung/galaxys4gmtd/proprietary/gps.s5pc110.so:system/vendor/lib/hw/gps.s5pc110.so \
     vendor/samsung/galaxys4gmtd/proprietary/rild:system/bin/rild \
     vendor/samsung/galaxys4gmtd/proprietary/gpsd:system/vendor/bin/gpsd \
