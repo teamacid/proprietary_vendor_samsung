3f4c776653e1afee85d88558938b7b74e53792a2
airfluip1 <airfluip1@yahoo.com>
2013-06-22 22:50:02 -0500

    derp

--- galaxys4gmtd/galaxys4gmtd-vendor-blobs.mk
+++ galaxys4gmtd/galaxys4gmtd-vendor-blobs.mk
@@ -21,7 +21,7 @@ PROCDUCT_COPY_FILES += \
 # All the other proprietary blobs necessary for a working SGS4G
 PRODUCT_COPY_FILES += \
     vendor/samsung/galaxys4gmtd/proprietary/etc/vendor/samsung-agps/secgps.conf:system/etc/vendor/samsung-agps/secgps.conf \
-    vendor/samsung/galaxys4gmtd/propreitary/etc/gps.xml:system/etc/gps.xml \
+    vendor/samsung/galaxys4gmtd/proprietary/etc/gps.xml:system/etc/gps.xml \
     vendor/samsung/galaxys4gmtd/proprietary/vendor/lib/hw/gps.aries.so:system/vendor/lib/hw/gps.aries.so \
     vendor/samsung/galaxys4gmtd/proprietary/vendor/bin/samsung-gpsd:system/vendor/bin/samsung-gpsd \
     vendor/samsung/galaxys4gmtd/proprietary/lib/libclientgps.so:system/lib/libclientgps.so \
